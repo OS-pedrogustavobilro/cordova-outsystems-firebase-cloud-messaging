15f48da389f8be638145b779f11788ee474453db
Mark Anderson <mark.anderson@outsystems.com>
2026-05-06 13:03:07 -0400

    add header checks

--- src/ios/AppDelegate+OSFirebaseCloudMessaging.h
+++ src/ios/AppDelegate+OSFirebaseCloudMessaging.h
@@ -1,4 +1,10 @@
-#import <Cordova/AppDelegate.h>
+#if __has_include("AppDelegate.h")
+    #import "AppDelegate.h"
+#else
+    #if __has_include(<Cordova/AppDelegate.h>)
+        #import <Cordova/AppDelegate.h>
+    #endif
+#endif
 
 @interface AppDelegate (OSFirebaseCloudMessaging) <UIApplicationDelegate>
 @end
