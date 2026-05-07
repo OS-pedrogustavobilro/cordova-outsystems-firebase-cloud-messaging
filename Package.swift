219411933f56795539e8608ec24609d2c8a395e1
Mark Anderson <emer@emer.net>
2026-05-07 09:10:55 -0400

    Update Package.swift
    Co-authored-by: Pedro Bilro <pedro.gustavo.bilro@outsystems.com>

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "com.outsystems.firebase.cloudmessaging",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "com.outsystems.firebase.cloudmessaging",
