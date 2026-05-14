4700a955682f234e703d2f685a6ec6a7b7b57b37
Mark Anderson <emer@emer.net>
2026-05-14 14:11:28 -0400

    Add an explanation of the strange appdelegate imports

--- src/ios/AppDelegate+OSFirebaseCloudMessaging.h
+++ src/ios/AppDelegate+OSFirebaseCloudMessaging.h
@@ -1,3 +1,7 @@
+// The old plugin made the assumption that AppDelegate existed
+// and was written in Objective-C. Since the Capacitor template
+// defaults to `AppDelegate.swift` we piggy back on Cordova's
+
 #if __has_include("AppDelegate.h")
     #import "AppDelegate.h"
 #else
